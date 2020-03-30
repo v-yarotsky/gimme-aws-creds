9043fdb84b1c382e0be1887f7052ecd85fe071b6
Justin Wiley <justin.wiley@nike.com>
2020-03-30 13:12:46 -0700

    This should do it, hopefully.

--- bin/gimme-aws-creds.cmd
+++ bin/gimme-aws-creds.cmd
@@ -12,7 +12,7 @@ for %%i in (cmd bat exe) do (
 for /f "tokens=2 delims==" %%i in ('assoc .py') do (
     for /f "tokens=2 delims==" %%j in ('ftype %%i') do (
         for /f "tokens=1" %%k in ("%%j") do (
-            call :SetPythonExe %%k
+            call :SetPythonExe "%%k"
         )
     )
 )
